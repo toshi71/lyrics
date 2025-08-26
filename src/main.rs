b248885687dc375694ba959f6e028530b1b811cf
中村俊英 <toshihident@hotmail.com>
2025-08-26 21:20:23 +0900

    アプリ名変更

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         viewport: eframe::egui::ViewportBuilder::default()
             .with_inner_size([320.0, 240.0])
-            .with_title("Hello World GUI"),
+            .with_title("FLAC Music Player"),
         ..Default::default()
     };
     
     eframe::run_native(
-        "Hello World GUI",
+        "FLAC Music Player",
         options,
         Box::new(|cc| {
             let app = MyApp::new();
